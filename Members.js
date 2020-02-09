74180d29ea2f37095a71573f37eec58d4db8e1d5
Martino NR <rantarou.uai@gmail.com>
2020-02-09 20:04:59 +0700

    first init

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'johndoe@email.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        email: 'janedoe@email.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Russel Doe',
+        email: 'russeldoe@email.com',
+        status: 'active'
+    },
+];
+
+module.exports = members;
